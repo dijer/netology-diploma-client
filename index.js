f04d646abea4a2a12a578ed7135f0a473ebf557a
Тимур Ахметзянов <dijer@mail.ru>
2021-03-07 12:01:05 +0300

    chat client example

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const { User } = require('./models');
+require('dotenv').config();
 const { SERVER_URL, SERVER_PORT } = require('./settings');
 const socketIO = require('socket.io-client');
 const readline = require('readline');
-const fs = require('fs');
-const path = require('path');
-
-const logFile = 'file.log';
-const filepath = path.join(__dirname, logFile);
+const axios = require('axios');
 
 const input = readline.createInterface(process.stdin, process.stdout);
-input.question('Enter your username?\n', username => {
-    const socket = socketIO.connect(`${SERVER_URL}:${SERVER_PORT}`, {
-        reconnect: true,
-    });
 
-    socket.on('connect', socket => {
-        console.log(`Join chat as ${username}`);
-    });
+input.question('Enter your username?\n', email => {
+    input.question('Enter your password', async password => {
+        const res = await axios.post(`${SERVER_URL}:${SERVER_PORT}/api/signin`, {
+            email,
+            password,
+        });
 
-    socket.on('message', message => {
-        console.log(message);
-        fs.appendFile(filepath, `${message}\n`, (err) => {
-            if (err) throw new Error(err);
-        })
-    });
-    socket.on('disconnect', socket => {
-        console.log('Leave chat');
-    });
+        const { data } = res.data;
 
-    const user = new User(username);
-    
-    input.on('line', message => {
-        if (message === '/exit') {
+        if (data.error) {
+            console.log(error);
             socket.close();
             input.close();
         }
-        socket.emit('message', user.say(message));
+        
+        const { id: userId } = data;
+        console.log(data);
+        const socket = socketIO.connect(`${SERVER_URL}:${SERVER_PORT}`, {
+            reconnect: true,
+            query: `userId=${userId}`,
+        });
+
+        socket.on('connect', socket => {
+            // console.log(`Join chat as ${username}`);
+        });
+
+        socket.on('message', message => {
+            console.log(message);
+        });
+        socket.on('disconnect', socket => {
+            console.log('Leave chat');
+        });
+
+        // const user = new User(username);
+        
+        input.on('line', message => {
+            if (message === '/exit') {
+                socket.close();
+                input.close();
+            }
+            socket.emit('sendMessage', userId, message);
+        });
     });
 });
