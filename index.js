e4416a5e95004a2057f8f28a6d30d06e96686735
Тимур Ахметзянов <dijer@mail.ru>
2021-03-09 22:40:05 +0300

    add newMessage recieve

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ const axios = require('axios');
 const input = readline.createInterface(process.stdin, process.stdout);
 
 input.question('Enter your username?\n', email => {
-    input.question('Enter your password', async password => {
-        const res = await axios.post(`${SERVER_URL}:${SERVER_PORT}/api/signin`, {
+    input.question('Enter your password\n', async password => {
+        const { data, headers } = await axios.post(`${SERVER_URL}:${SERVER_PORT}/api/signin`, {
             email,
             password,
         });
 
-        const { data } = res.data;
-
         if (data.error) {
-            console.log(error);
-            socket.close();
+            console.log(data.error);
             input.close();
+            return;
         }
 
-        const cookie = res.headers['set-cookie'];
+        const cookie = headers['set-cookie'];
         
         const socket = socketIO.connect(`${SERVER_URL}:${SERVER_PORT}`, {
             reconnect: true,
@@ -34,7 +32,7 @@ input.question('Enter your username?\n', email => {
             // console.log(`Join chat as ${username}`);
         });
 
-        socket.on('message', message => {
+        socket.on('newMessage', message => {
             console.log(message);
         });
         socket.on('disconnect', socket => {
@@ -45,7 +43,7 @@ input.question('Enter your username?\n', email => {
             messages.forEach(message => console.log(message.text));
         });
         
-        input.question('Reciever email:', recieverEmail => {
+        input.question('Reciever email:\n', recieverEmail => {
             const history = socket.emit('getHistory', recieverEmail);
             input.on('line', message => {
                 if (message === '/exit') {
